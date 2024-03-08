00958e3924bdee58ca1f19dfb6eb8bff5e71f986
hassen <hassen@hassen.com>
2024-03-08 20:46:18 +0900

    initial code upload

---
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class MainController {
+    @GetMapping("/sbb")
+    public void index(){
+        System.out.println("index");
+        
+    }
+    
+}
